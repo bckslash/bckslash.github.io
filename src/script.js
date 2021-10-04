7e39df479eb987fbdee60308e2c30ad3160fd1cd
bckslash <exploreeune@gmail.com>
2021-10-04 16:12:15 +0200

    project

---
+++ src/script.js
@@ -0,0 +1,15 @@
+const btn_theme = document.querySelector(".btn-theme");
+
+document.body.classList.remove();
+document.body.classList.add(localStorage.getItem("theme"));
+
+btn_theme.addEventListener("click", () => {
+	if (document.body.classList.contains("light")) {
+		document.body.classList.replace("light", "dark");
+		localStorage.setItem("theme", "dark");
+	} else {
+		document.body.classList.replace("dark", "light");
+		localStorage.setItem("theme", "light");
+	}
+	btn_theme.classList.toggle("dark");
+});
